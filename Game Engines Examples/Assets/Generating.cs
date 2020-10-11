5bdc5e74c375fb56af1811042528c363244726ef
EmpireofZakuul <60693663+EmpireofZakuul@users.noreply.github.com>
2020-10-11 13:23:55 +0100

    sd

---
+++ Game Engines Examples/Assets/Generating.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generating : MonoBehaviour
+{
+    public int circleLoops 2;
+    public GameObject GameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+ 
+}
